cf1dce97d6d357d4b8c83287c0e7fee52ba2ab13
yudhaputraa <elekronikayudha@gmail.com>
2022-05-12 09:28:34 +0700

    penjelasan awal

---
+++ stack_linked_list.py
@@ -0,0 +1,5 @@
+"""
+    Stack merupakan salah satu implementasi dari Linked List. Stack merupakan kumpulan-kumpulan data yang 
+menggunakan konsep LIFO ( Last In First Out ) atau FILO ( First In Last Out ), yaitu data yang paling 
+terakhir dimasukan ke dalam stack merupakan data yang pertama kali keluar dari stack .
+"""
