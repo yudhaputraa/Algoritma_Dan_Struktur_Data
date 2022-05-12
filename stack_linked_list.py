83b75b3bd63f0d3d5934f4be3a21b4d634ac6ed9
yudhaputraa <elekronikayudha@gmail.com>
2022-05-12 09:35:04 +0700

    menambahkan kelas node

--- stack_linked_list.py
+++ stack_linked_list.py
@@ -3,3 +3,9 @@
 menggunakan konsep LIFO ( Last In First Out ) atau FILO ( First In Last Out ), yaitu data yang paling 
 terakhir dimasukan ke dalam stack merupakan data yang pertama kali keluar dari stack .
 """
+
+class node:
+    def __init__(self, element, next=None):
+        self.element = element
+        self.next = next
+
