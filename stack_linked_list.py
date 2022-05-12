ca7807c3b827082c333653963db29a0bba5031d5
yudhaputraa <elekronikayudha@gmail.com>
2022-05-12 09:44:23 +0700

    menambahkan kelas llstack(__init__)

--- stack_linked_list.py
+++ stack_linked_list.py
@@ -9,3 +9,10 @@ class node:
         self.element = element
         self.next = next
 
+class llstack:
+    def __init__(self):
+        # refensi yg menujukan ke simpul pertama
+        self.head = None
+        # referensi yg menujukan ke simpul terakhir
+        self.tail = None
+        self.size = 0   # mula-mula stack kosong
